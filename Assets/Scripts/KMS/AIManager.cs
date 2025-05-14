0b43391391ee31f3e5da9d844067f6022a29b96a
KimMinSung235 <alexkim0638@gmail.com>
2025-05-15 04:26:15 +0900

    About Gemini 2.0 flash
    Modifing prompt

--- Assets/Scripts/KMS/AIManager.cs
+++ Assets/Scripts/KMS/AIManager.cs
@@ -5,24 +5,38 @@ using System.Text;
 
 public class GeminiChat : MonoBehaviour
 {
+    private int gameOverCount;//지금까지 몇번 실패했는지
+    private int lastStageInfo;//이전 게임에서 스테이지 몇에서 게임오버했는지.
+
     [Header("Gemini2.0 flash API Key")]
     public string apiKey = "AIzaSyCJsIsZdQpYYUegM_9bO5UXKKtBARP60lk";
-
-    [TextArea]
-    public string userInput = @"
-당신은 엘렌이라는 NPC입니다. 말투는 고상하고 조용하며, 대화 시 항상 비유를 섞습니다.
-
-게임 정보:
-- 게임 시간: 3일차
-- 플레이어 직업: 전사
-- 진행 지역: 엘다 숲
-- 현재 퀘스트: 사라진 유물 찾기
-- 소지 아이템: 고대의 열쇠, 해독제
-
-플레이어가 묻습니다: 엘렌, 이 유물은 어디서 찾아야 하나요?";
-
-    public void requestGemini(){
-        StartCoroutine(SendGeminiRequest(userInput));
+    private string inputText = @"";
+    public void SetGameOverCount(int gameOverCount){
+        this.gameOverCount = gameOverCount;
+    }
+    public void SetLastStageInfo(int lastStageInfo){
+        this.lastStageInfo = lastStageInfo;
+    }
+    void Start()
+    {
+        SetGameOverCount(6);
+        SetLastStageInfo(7);
+        RequestGemini();
+    }
+    public void RequestGemini(){ //inputText만들고 요청하기
+        inputText = @$"
+        게임오버횟수:{gameOverCount}번
+        직전스테이지:{lastStageInfo}층
+        npc정보:
+        (세계관 배경은 중세판타지이고, 계속해서 도전하는 플레이어에게 항상 문을 열어주는 문지기, 장난끼 많고 놀리는걸 좋아하여 실패한 플레이어를 놀린다.
+        갑옷으로 온몸을 가린 남성이며 누구에게나 반말을 쓴다.)
+        응답형식:
+        (npc의 입장에서 대답을 해줘야 한다. 또한 대답 형식은 여러줄로 해주어야 하고 줄바꿈은 \n으로 해줘야 한다.
+        게임오버횟수만큼 실패하였다는 언급, 직전스테이지만큼의 층까지 도달하였다는 언급은 필수이다. 
+        직저스테이지에 따라 어느정도 잘했는지 못했는지에 대한 반응을 보여준다.(던전은 총 9층으로 이에 대한 평가를 해준다. 다만 던전이 9층이라는 것은 비밀이다.)
+        직전스테이지가 8,9층의 경우 놀리다가도 그 직후에 너라면 할 수 있을거라는 격려를 해준다.
+        그리고 대화의 마지막엔 이제 문을 열어주겠다는 언급과 함께 항상 행운을 빈다고 말해줘야 한다.)";
+        StartCoroutine(SendGeminiRequest(inputText));
     }
 
     IEnumerator SendGeminiRequest(string prompt)
